803e522d4fec7938421ca8bbb7d9d44ee20e0696
bijiajo <bijibabu1791@gmail.com>
2023-07-30 16:27:21 +0530

    firsy commit

---
+++ todo/todo/todoapplicationproject/todoprojectapp/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+
+    path('', views.add, name='add'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update')
+
+]
